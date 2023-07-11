d1bbaaa1e50098254a6c56bf3005c1b715bdd002
JHbarreto <jose.henrique.paes.barreto@gmail.com>
2023-07-11 04:18:28 -0300

    adicionando a logica de funcionamento para o conversor de medidas

--- src/PrimeiraTela.java
+++ src/PrimeiraTela.java
@@ -4,16 +4,17 @@ import java.text.DecimalFormat;
 
 
 public class PrimeiraTela extends JFrame {
-    private JPanel panel1;
+    private JPanel panelPrincipal;
     private JButton confirmarButton;
     private JButton sairButton;
     private JComboBox<String> optionBox;
 
     public PrimeiraTela() {
-        setContentPane(panel1);
+        setContentPane(panelPrincipal);
         setTitle("Menu Inicial");
-        setSize(250, 250);
+        setSize(250, 120);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         setVisible(true);
 
         String[] opcoes = {"conversor de moeda", "conversor de temperatura", "conversor de medida"};
@@ -87,13 +88,13 @@ public class PrimeiraTela extends JFrame {
 
             case "conversor de temperatura" -> {
                 boolean valorIncorreto = true;
-                double valor = 0; // Inicialize o valor com um valor padrão
+                double valor = 0;
 
                 while (valorIncorreto) {
                     String valorString = JOptionPane.showInputDialog(null, "Insira um valor:", "Insira um valor", JOptionPane.INFORMATION_MESSAGE);
                     try {
                         valor = Double.parseDouble(valorString);
-                        valorIncorreto = false; // Defina valorIncorreto como false se o valor for válido
+                        valorIncorreto = false;
                     } catch (NumberFormatException ex) {
                         JOptionPane.showMessageDialog(null, "Valor incorreto, digite novamente (apenas números)", "Valor incorreto", JOptionPane.WARNING_MESSAGE);
                     }
@@ -120,13 +121,62 @@ public class PrimeiraTela extends JFrame {
             }
 
             case "conversor de medida" -> {
-                double valor = Double.parseDouble(JOptionPane.showInputDialog(null, "insira um valor:", "insira um valor", JOptionPane.INFORMATION_MESSAGE));
-                if (valor != 0 && valor > 0) {
-                    String[] possibilidades = {"De m para km", "De Km a m", "De m a hm", "De hm a m", "De m a dam", "De dam a m", "De m a dm",
-                            "De dm a m", "De m a cm", "De cm a m", "De m a mm", "De mm a m"};
-                    JOptionPane.showInputDialog(null, "escolha a medida", "conversão", JOptionPane.QUESTION_MESSAGE, null, possibilidades, possibilidades[0]);
-                } else
-                    Double.parseDouble(JOptionPane.showInputDialog(null, "valor incorreto,digite novamente", "valor incorreto", JOptionPane.WARNING_MESSAGE));
+                double valor = 0;
+                boolean valorIncorreto = true;
+
+                while (valorIncorreto) {
+                    String valorString = JOptionPane.showInputDialog(null, "Insira um valor:", "Insira um valor", JOptionPane.INFORMATION_MESSAGE);
+
+                    try {
+                        valor = Double.parseDouble(valorString);
+                        if (valor != 0 && valor > 0) {
+                            valorIncorreto = false;
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Valor incorreto, digite novamente (diferente de zero / maior que zero)", "Valor incorreto", JOptionPane.WARNING_MESSAGE);
+                        }
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Valor incorreto, digite novamente (número inválido)", "Valor incorreto", JOptionPane.WARNING_MESSAGE);
+                    }
+                }
+
+                String[] possibilidades = {"De m para km", "De Km a m", "De m a hm", "De hm a m", "De m a dam", "De dam a m", "De m a dm",
+                        "De dm a m", "De m a cm", "De cm a m", "De m a mm", "De mm a m"};
+                String escolha = (String) JOptionPane.showInputDialog(null, "Escolha como você deseja converter :",
+                        "Conversão", JOptionPane.QUESTION_MESSAGE, null, possibilidades, possibilidades[0]);
+
+                int indiceEscolhido = -1;
+                double baseConversao = 10;
+                double[] expoenteConversao = {3,2,1,1,2,3};
+                double[] fatorConversao = new double[expoenteConversao.length];
+
+
+                for (int i = 0; i < expoenteConversao.length; i++) {
+                    fatorConversao[i] = Math.pow(baseConversao, expoenteConversao[i]);
+                }
+
+                for (int i = 0; i < possibilidades.length; i++) {
+                    if (escolha.equals(possibilidades[i])) {
+                        indiceEscolhido = i;
+                        break;
+                    }
+                }
+
+                if (indiceEscolhido != -1) {
+                    double resultado = 0;
+
+                    if (indiceEscolhido % 2 == 0 && indiceEscolhido <= 5) {
+                        resultado = valor / fatorConversao[indiceEscolhido / 2];
+                    } else if (indiceEscolhido % 2 != 0 && indiceEscolhido <= 5) {
+                        resultado = valor * fatorConversao[indiceEscolhido / 2];
+                    } else if (indiceEscolhido % 2 == 0 && indiceEscolhido > 5) {
+                        resultado = valor * fatorConversao[indiceEscolhido / 2];
+                    } else {
+                        resultado = valor / fatorConversao[indiceEscolhido / 2];
+                    }
+
+
+                    JOptionPane.showMessageDialog(null, "Resultado: " + (resultado), "Resultado", JOptionPane.INFORMATION_MESSAGE, null);
+                }
             }
         }
 
